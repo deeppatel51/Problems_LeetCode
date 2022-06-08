be2d0b94ff3e24366dfd4bb04e5584994615e844
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-06-08 20:01:24 +0530

    Update Array_SortAnArray.cpp

--- Array_SortAnArray.cpp
+++ Array_SortAnArray.cpp
@@ -1,43 +1,41 @@
 class Solution {
 public:
-    // int partition(vector<int> &nums, int l, int h)
-    // {
-    //     int p=nums[l];
-    //     int i=l;
-    //     int j=h;
-    //     while(i<j)
-    //     {
-    //         do
-    //         {
-    //             i++;
-    //         }while(nums[i]<=p);
-    //         do
-    //         {
-    //             j--;
-    //         }while(nums[j]>p);
-    //         if(i<j)
-    //         {
-    //             swap(nums[i],nums[j]);
-    //         }
-    //     }swap(nums[l],nums[j]);
-    //     return j;
-    // }
-    // vector<int> quickSort(vector<int> &nums, int l, int h)
-    // {
-    //     if(l<h)
-    //     {
-    //         int j = partition(nums, l, h);
-    //         quickSort(nums, l,j);
-    //         quickSort(nums, j+1,h);
-    //     }
-    //     return nums;
-    // }
+    int partition(vector<int> &nums, int l, int h)
+    {
+        int p=nums[l];
+        int i=l;
+        int j=h;
+        while(i<j)
+        {
+            do
+            {
+                i++;
+            }while(nums[i]<=p);
+            do
+            {
+                j--;
+            }while(nums[j]>p);
+            if(i<j)
+            {
+                swap(nums[i],nums[j]);
+            }
+        }swap(nums[l],nums[j]);
+        return j;
+    }
+    vector<int> quickSort(vector<int> &nums, int l, int h)
+    {
+        if(l<h)
+        {
+            int j = partition(nums, l, h);
+            quickSort(nums, l,j);
+            quickSort(nums, j+1,h);
+        }
+        return nums;
+    }
     vector<int> sortArray(vector<int>& nums) {
-        // nums.push_back(60000);
-        // nums = quickSort(nums, 0,nums.size()-1);
-        // nums.pop_back();
-        // return nums;
-        sort(nums.begin(),nums.end());
+        nums.push_back(60000);
+        nums = quickSort(nums, 0,nums.size()-1);
+        nums.pop_back();
         return nums;
     }
         
