e59a53481abee30ed9e9148e6b83bbce925e41ce
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-06-09 16:51:48 +0530

    Update Array_MinimumTimeDifference.cpp

--- Array_MinimumTimeDifference.cpp
+++ Array_MinimumTimeDifference.cpp
@@ -3,14 +3,16 @@ public:
     int findMinDifference(vector<string>& timePoints) {
         int n = timePoints.size();
         vector<int> time(n,0);
-        for(int i = 0; i < n;i++){
+        for(int i = 0; i < n;i++)
+        {
             string h = timePoints[i].substr(0,2);
             string m = timePoints[i].substr(3,2);
             time[i] = stoi(h)*60+stoi(m);
         }
         sort(time.begin(),time.end());
         int mini = INT_MAX;
-        for(int i = 1; i < n;i++){
+        for(int i = 1; i < n;i++)
+        {
             int diff = time[i]-time[i-1];
             mini = min(mini,diff);
         }
