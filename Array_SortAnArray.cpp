2563e459b01f65e86b2096a072d447102ad0d0aa
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-06-09 15:23:41 +0530

    Update Array_SortAnArray.cpp

--- Array_SortAnArray.cpp
+++ Array_SortAnArray.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int partition(vector<int> &nums, int l, int h)
+    int partition(vector<int>& nums, int l, int h)
     {
         int p=nums[l];
         int i=l;
@@ -22,7 +22,7 @@ public:
         }swap(nums[l],nums[j]);
         return j;
     }
-    vector<int> quickSort(vector<int> &nums, int l, int h)
+    void quickSort(vector<int>& nums, int l, int h)
     {
         if(l<h)
         {
@@ -30,13 +30,21 @@ public:
             quickSort(nums, l,j);
             quickSort(nums, j+1,h);
         }
-        return nums;
     }
     vector<int> sortArray(vector<int>& nums) {
-        nums.push_back(60000);
-        nums = quickSort(nums, 0,nums.size()-1);
+        bool flag=false;
+        for(int i=0;i<nums.size()-1;i++){
+            if(nums[i]>nums[i+1]){
+                flag=true;
+                break;
+            }
+        }
+        if(flag)
+        {
+        nums.push_back(INT_MAX);
+        quickSort(nums, 0,nums.size()-1);
         nums.pop_back();
+        }
         return nums;
-    }
-        
+    }     
 };
