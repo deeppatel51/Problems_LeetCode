21a745496fbad3c469e91951d3c942f2911467ec
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-06-10 14:00:12 +0530

    Create Array_NextPermutation.cpp

---
+++ Array_NextPermutation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int ind1,ind2;
+        int count = 0;
+        for(int i=nums.size()-1;i>0;i--)
+        {
+            if(nums[i]>nums[i-1])
+            {
+                ind1 = i-1;
+                count++;
+                break;
+            }
+        }
+        if(count == 0)
+        {
+            reverse(nums.begin(),nums.end());
+        }
+        else
+        {
+            for(int i=nums.size()-1;i>0;i--)
+            {
+                if(nums[i]>nums[ind1])
+                {
+                    ind2 = i;
+                    break;
+                }
+            }
+            swap(nums[ind1],nums[ind2]);
+            reverse(nums.begin()+ind1+1,nums.end());
+        }
+        return;
+    }
+};
