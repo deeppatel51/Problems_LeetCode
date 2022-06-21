676be151b9d0f9ccecf46f6061783074358b5471
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-06-21 20:50:13 +0530

    Create Searching&Sorting_FindNearestPointThatHasTheSameXOrYCoordinate.cpp

---
+++ Searching&Sorting_FindNearestPointThatHasTheSameXOrYCoordinate.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+        int pos = -1;
+        int ans = INT_MAX;
+        for(int i = 0; i < points.size();i++)
+        {
+            if(x == points[i][0] || y == points[i][1])
+            {
+                int dist = abs(x-points[i][0]) + abs(y-points[i][1]);
+                if(dist < ans)
+                {
+                    ans = dist;
+                    pos = i;
+                }           
+            }
+        }
+        return pos;
+    }
+};
