daf82aad313ee1a50b366fa61787c02c13f22f20
Deep Patel <88723958+deeppatel51@users.noreply.github.com>
2022-10-06 10:30:27 +0530

    Create LinkedList_SwappingNodesinaLinkedList.cpp

---
+++ LinkedList_SwappingNodesinaLinkedList.cpp
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* swapNodes(ListNode* head, int k) {
+	vector<ListNode*> vNodes;
+	ListNode* cur = head;
+	while (cur) {
+		vNodes.push_back(cur);
+		cur = cur->next;
+	}
+	swap(vNodes[k - 1]->val, vNodes[vNodes.size() - k]->val);
+	return head;
+}
+};
